6776f9be89f60a4cad09fdf1496708d431147f47
lgklsv <kolosov20011@gmail.com>
2025-06-12 08:52:02 +0400

    Allow github actions iam role for pull requests

--- roles.tf
+++ roles.tf
@@ -13,7 +13,7 @@ resource "aws_iam_role" "github_actions_role" {
         Condition = {
           StringEquals = {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-            "token.actions.githubusercontent.com:sub" = "repo:lgklsv/aws-devops-2025-q2:ref:refs/heads/*"
+            "token.actions.githubusercontent.com:sub" = "repo:lgklsv/aws-devops-2025-q2:*"
           }
         }
       },
